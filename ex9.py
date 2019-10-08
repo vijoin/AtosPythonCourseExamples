c456b6136614f4ac37e31c819a3be601549c15a9
Victor Inojosa <vijoin@gmail.com>
2019-10-07 22:10:53 -0300

    DICTIONARY Improve exercises

--- ex9.py
+++ ex9.py
@@ -1,46 +1,61 @@
 def print_dictionaries():
-    Countries = {'Uruguay':
-                  {'official_name': 'República Oriental del Uruguay',
-                   'capital': 'Montevideo',
-                   'population': 3_457_000,
-                   }
-              }
+    #Add a country
+    countries = {'Uruguay':
+                     {'official_name': 'República Oriental del Uruguay',
+                      'capital': 'Montevideo',
+                      'population': 3_457_000,
+                      }
+                 }
 
-    print(Countries)
+    print(countries)
 
-    Countries['Venezuela'] = {'official_name': 'República Bolivariana de Venezuela',
-                           'capital': 'Caracas',
-                           'population': 31_980_000,
-                           }
-    print(Countries)
-    print(Countries['Venezuela'])
-    print(Countries['Uruguay'])
-    print(Countries['Uruguay']['population'])
-    print(Countries['Venezuela']['capital'])
+    #Add a second key
+    countries['Venezuela'] = {'official_name': 'República Bolivariana de Venezuela',
+                              'capital': 'Caracas',
+                              'population': 31_980_000,
+                              }
+    print(countries)
+
+
+    #access to a specific key
+    print(countries['Venezuela'])
+    print(countries['Uruguay'])
+
+    #Access multilevel key
+    print(countries['Uruguay']['population'])
+    print(countries['Venezuela']['capital'])
 
     country = 'Venezuela'
-    print("The Capital of {} is {}".format(country, Countries[country]['capital']))
+    print("The Capital of {} is {}".format(country, countries[country]['capital']))
 
     #What if I ask for a non existing key
     country = 'Uruguay'
     #language = Countries[country]['language']
-    language = Countries[country].get('language', 'Not Specified')
+
+    ## I can catch the error with the get() method
+    language = countries[country].get('language', 'Not Specified')
     print(f'The Population of {country} is {language}')
 
-    population = Countries[country].get('population', 'NULL')
+    population = countries[country].get('population', 'NULL')
     print(f'The Population of {country} is {population}')
 
-    countries_keys = Countries.keys()
-    countries_values = Countries.values()
-    countries_items = Countries['Uruguay'].items()
-
+    #Access to the keys
+    countries_keys = countries.keys()
     print(countries_keys)
+
+    #Access to the values
+    countries_values = countries.values()
     print(countries_values)
+
+    #Access both keys and items
+    countries_items = countries['Uruguay'].items()
+
     print(countries_items)
 
-    del Countries['Venezuela']['population']
-    print(Countries['Venezuela'])
+    del countries['Venezuela']['population']
+    print(countries['Venezuela'])
 
+    print("Los datos de Venezuela son {official_name}, {capital}, {population}".format(**countries['Venezuela']))
 
 
 if __name__ == '__main__':
